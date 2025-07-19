3f8a8ebc4af8faba0d47cda4ecce307c43807ec5
mega <mega@DESKTOP-HAFNPP1>
2025-07-20 00:43:16 +0100

    faza4

--- Scripts/Demo/gitdemo/Script1752967769266.groovy
+++ Scripts/Demo/gitdemo/Script1752967769266.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.delay(5)
+WebUI.delay(7)
 
 WebUI.delay(5)
 
